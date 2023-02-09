153cf12147f563a3d134c3fd05f4515b39d450ef
Rao Muhammad Faran Latif <124181306+Faran005@users.noreply.github.com>
2023-02-09 21:59:13 +0500

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int table;
+      int n=1;
+      cin>>table;
+for(int n= 1; n<=10;n++){
+	cout<<"Table= "<<table*n<<endl;
+	
+}
+return 0;
+}
